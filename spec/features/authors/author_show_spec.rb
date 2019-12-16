5b56e39858c9b6a4e7877a28a22af315d8dc846f
msimon42 <msimon@uab.edu>
2019-12-16 13:05:39 -0700

    Add to author show

--- spec/features/authors/author_show_spec.rb
+++ spec/features/authors/author_show_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'author show page', type: :feature do
   before :each do
-    Author.destroy_all
 
     @author_1 = Author.create(
       name: 'Albus Dumbledore'
@@ -11,11 +10,20 @@ RSpec.describe 'author show page', type: :feature do
     @author_2 = Author.create(
       name: 'Harry Potter'
     )
+
+    @book_1 = Book.create(
+      title: 'The End of Time',
+      publication_year: '1966',
+      pages: 367
+    )
+
+    @author_1.books << @book_1
   end
 
   it 'can show author information' do
     visit "/authors/#{@author_1.id}"
 
     expect(page).to have_content(@author_1.name)
+    expect(page).to have_content(@book_1.title)
   end
 end
